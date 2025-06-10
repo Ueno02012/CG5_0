654c9a508fcda4d98896577d40463aea64983d91
Ueno02012 <k023g0064@g.neec.ac.jp>
2025-06-10 20:07:08 +0900

    完了

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -56,85 +56,85 @@ void SetupPipelineState(PipelineState& pipelineState, RootSignature& rs, Shader&
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	
-KamataEngine::Initialize(L"LE3D_02_ウエノ_ユウキ");
-
-// DirectXCommonのインスタンスの取得
-DirectXCommon* dxCommon = DirectXCommon::GetInstance();
-
-// DirectXCommonクラスが取得している、ウィンドウの幅と高さの値の取得
-int32_t w = dxCommon->GetBackBufferWidth();
-int32_t h = dxCommon->GetBackBufferHeight();
-DebugText::GetInstance()->ConsolePrintf(
-	std::format("width: {},height: {}\n", w, h).c_str());
-
-// DirectXCommonクラスが管理している、コマンドリストの取得
-
-ID3D12GraphicsCommandList* commandList = dxCommon->GetCommandList(); 
-
-//-----------------RootSignatureの作成-----------------//
-RootSignature rs;
-rs.Create();
-
-//------------------VertexShaderをCompileする------------------//
-// コンパイル済みのShader、エラー時情報の格納場所の用意
-ID3DBlob* errorBlob = nullptr; //エラーオブジェクト
-
-//　頂点シェーダの読み込みとコンパイル
-Shader vs;
-vs.LoadDxc(L"Resources/shaders/TestVS.hlsl", L"vs_6_0"); // 変更
-assert(vs.GetDxcBlob() != nullptr); 
- 
-//------------------PixelShaderをCompileする------------------//
-// ピクセルシェーダの読み込みとコンパイル
-Shader ps;
-ps.LoadDxc(L"Resources/shaders/TestPS.hlsl", L"ps_6_0"); // 変更
-assert(ps.GetDxcBlob() != nullptr);                      
-//------------------PSOの生成------------------//
-PipelineState pipelineState;
-SetupPipelineState(pipelineState, rs, vs, ps);
-
-//------------------VertexResourceを生成する------------------//
-VertexBuffer vb;
-vb.Create(sizeof(Vector4) * 3, sizeof(Vector4));
-
-//------------------頂点リソースにデータを書き込む------------------//
-// 頂点リソースにデータを書き込む
-Vector4* vertexData = nullptr;
-vb.Get()->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
-vertexData[0] = {-0.5f, -0.5f, 0.0f, 1.0f}; // 左下
-vertexData[1] = {0.0f, 0.5f, 0.0f, 1.0f};   // 上
-vertexData[2] = {0.5f, -0.5f, 0.0f, 1.0f};  // 右下
-// 頂点リソースのマップを解除する
-vb.Get()->Unmap(0, nullptr);
-
-// メインループ
-while (true) {
-	//エンジンの更新
-	if (KamataEngine::Update()) {
-		break;
+	KamataEngine::Initialize(L"LE3D_02_ウエノ_ユウキ");
+	
+	// DirectXCommonのインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+	
+	// DirectXCommonクラスが取得している、ウィンドウの幅と高さの値の取得
+	int32_t w = dxCommon->GetBackBufferWidth();
+	int32_t h = dxCommon->GetBackBufferHeight();
+	DebugText::GetInstance()->ConsolePrintf(
+		std::format("width: {},height: {}\n", w, h).c_str());
+	
+	// DirectXCommonクラスが管理している、コマンドリストの取得
+	
+	ID3D12GraphicsCommandList* commandList = dxCommon->GetCommandList(); 
+	
+	//-----------------RootSignatureの作成-----------------//
+	RootSignature rs;
+	rs.Create();
+	
+	//------------------VertexShaderをCompileする------------------//
+	// コンパイル済みのShader、エラー時情報の格納場所の用意
+	ID3DBlob* errorBlob = nullptr; //エラーオブジェクト
+	
+	//　頂点シェーダの読み込みとコンパイル
+	Shader vs;
+	vs.LoadDxc(L"Resources/shaders/TestVS.hlsl", L"vs_6_0"); // 変更
+	assert(vs.GetDxcBlob() != nullptr); 
+	 
+	//------------------PixelShaderをCompileする------------------//
+	// ピクセルシェーダの読み込みとコンパイル
+	Shader ps;
+	ps.LoadDxc(L"Resources/shaders/TestPS.hlsl", L"ps_6_0"); // 変更
+	assert(ps.GetDxcBlob() != nullptr);                      
+	//------------------PSOの生成------------------//
+	PipelineState pipelineState;
+	SetupPipelineState(pipelineState, rs, vs, ps);
+	
+	//------------------VertexResourceを生成する------------------//
+	VertexBuffer vb;
+	vb.Create(sizeof(Vector4) * 3, sizeof(Vector4));
+	
+	//------------------頂点リソースにデータを書き込む------------------//
+	// 頂点リソースにデータを書き込む
+	Vector4* vertexData = nullptr;
+	vb.Get()->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
+	vertexData[0] = {-0.5f, -0.5f, 0.0f, 1.0f}; // 左下
+	vertexData[1] = {0.0f, 0.5f, 0.0f, 1.0f};   // 上
+	vertexData[2] = {0.5f, -0.5f, 0.0f, 1.0f};  // 右下
+	// 頂点リソースのマップを解除する
+	vb.Get()->Unmap(0, nullptr);
+	
+	// メインループ
+	while (true) {
+		//エンジンの更新
+		if (KamataEngine::Update()) {
+			break;
+		}
+		//-------------------------- 描画
+		// 描画開始
+		dxCommon->PreDraw();
+	
+		// コマンドを読む
+		commandList->SetGraphicsRootSignature(rs.Get());     // RootSignatureの設定
+		commandList->SetPipelineState(pipelineState.Get());  // PSOの設定をする
+		commandList->IASetVertexBuffers(0, 1, vb.GetView()); // VBVの設定をする	// トポロジの設定
+		commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+		// 頂点数、インデックス数、インデックスの開始位置、インデックスのオフセット
+		commandList->DrawInstanced(3, 1, 0, 0);
+	
+		// 描画終了
+		dxCommon->PostDraw();
 	}
-	//-------------------------- 描画
-	// 描画開始
-	dxCommon->PreDraw();
-
-	// コマンドを読む
-	commandList->SetGraphicsRootSignature(rs.Get());     // RootSignatureの設定
-	commandList->SetPipelineState(pipelineState.Get());  // PSOの設定をする
-	commandList->IASetVertexBuffers(0, 1, vb.GetView()); // VBVの設定をする	// トポロジの設定
-	commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-	// 頂点数、インデックス数、インデックスの開始位置、インデックスのオフセット
-	commandList->DrawInstanced(3, 1, 0, 0);
-
-	// 描画終了
-	dxCommon->PostDraw();
-}
-// 解放処理
-if (errorBlob) {
-	errorBlob->Release();
-}
-//エンジンの終了処理
-KamataEngine::Finalize();
-
+	// 解放処理
+	if (errorBlob) {
+		errorBlob->Release();
+	}
+	//エンジンの終了処理
+	KamataEngine::Finalize();
+	
 	return 0;
 }
 
