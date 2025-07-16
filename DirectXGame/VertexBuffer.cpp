e68dde87a32bf5bf10d0f14ccaa209a44ee3be94
Ueno02012 <k023g0064@g.neec.ac.jp>
2025-07-16 10:09:17 +0900

    完

--- DirectXGame/VertexBuffer.cpp
+++ DirectXGame/VertexBuffer.cpp
@@ -63,4 +63,9 @@ D3D12_VERTEX_BUFFER_VIEW* VertexBuffer::GetView()
 VertexBuffer::VertexBuffer() {}
 
 //デストラクタ
-VertexBuffer::~VertexBuffer() {}
+VertexBuffer::~VertexBuffer() {
+	if (vertexBuffer_) {
+		vertexBuffer_->Release();
+		vertexBuffer_ = nullptr;
+	}
+}
